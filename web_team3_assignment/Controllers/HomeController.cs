0bc7fdef366f8ed654b06616de832112075837cf
Jia Qi Poh <s10189672@connect.np.edu.sg>
2019-05-15 13:46:07 +0800

    Add project files.

---
+++ web_team3_assignment/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace web_team3_assignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult StaffLogin(IFormCollection formData)
+        {
+            // Read inputs from textboxes
+            // Email address converted to lowercase
+            string loginID = formData["txtLoginID"].ToString().ToLower();
+            string password = formData["txtPassword"].ToString();
+            if (loginID == "abc@npbook.com" && password == "pass1234")
+            {
+                // Redirect user to the "StaffMain" view through an action
+                return RedirectToAction("LecturerMain");
+            }
+            else
+            {
+                // Redirect user back to the index view through an action
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult LecturerMain()
+        {
+            return View();
+        }
+    }
+}
