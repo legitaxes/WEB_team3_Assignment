48bf338207efef95a3a1e482d66464cd4b0f8898
weilinchoo <s10189897@connect.np.edu.sg>
2019-06-18 20:56:51 +0800

    Project & ProjectMember Models

---
+++ web_team3_assignment/Models/Project.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace web_team3_assignment.Models
+{
+    public class Project
+    {
+        [Required]
+        [Display(Name = "Project ID")]
+        public int ProjectId { get; set; }
+
+        [Required]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Project Poster")]
+        public string ProjectPoster { get; set; }
+
+        [Required]
+        [Display(Name = "Project URL")]
+        public string ProjectURL { get; set; }
+    }
+}
---
+++ web_team3_assignment/Models/ProjectMember.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace web_team3_assignment.Models
+{
+    public class ProjectMember
+    {
+        [Required]
+        [Display(Name = "Project ID")]
+        public int ProjectId { get; set; }
+
+        [Required]
+        [Display(Name = "Student ID")]
+        public int StudentId { get; set; }
+
+        [Required]
+        [Display(Name = "Role")]
+        public int Role { get; set; }
+    }
+}
