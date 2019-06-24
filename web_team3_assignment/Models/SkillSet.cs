0d726dabc10c8300f5820f279293c4fe3bf9cdca
Mingyi Brandon Lim <s10189534@connect.np.edu.sg>
2019-06-24 11:30:43 +0800

    SkillSet Model

---
+++ web_team3_assignment/Models/SkillSet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace web_team3_assignment.Models
+{
+    public class SkillSet
+    {
+        [Key]
+        [Display(Name = "SkillSet ID")]
+        public int SkillSetId { get; set; }
+
+        [Required(ErrorMessage = "Please Do not Leave This Field Blank!")]
+        [StringLength(255, ErrorMessage = "Skill Set Name Cannot Exceed 255 Characters!")]
+        [Display(Name = "SkillSet Name")]
+        public string SkillSetName { get; set; }
+    }
+}
