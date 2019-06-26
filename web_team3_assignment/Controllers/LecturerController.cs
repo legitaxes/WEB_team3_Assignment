5213c4f960c15fa2cf1ddf1c14e25388547d2a33
Jia Qi Poh <s10189672@connect.np.edu.sg>
2019-06-27 00:02:33 +0800

    Added Basscss: https://basscss.com/v7/ to layout Created lecturer profile creation

--- web_team3_assignment/Controllers/LecturerController.cs
+++ web_team3_assignment/Controllers/LecturerController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using web_team3_assignment.DAL;
 using web_team3_assignment.Models;
 
@@ -35,6 +36,12 @@ namespace web_team3_assignment.Controllers
         // GET: Lecturer/Create
         public ActionResult Create()
         {
+            // Stop accessing the action if not logged in // or account not in the "Lecturer" role
+            if ((HttpContext.Session.GetString("Role") == null) || 
+                (HttpContext.Session.GetString("Role") != "Lecturer"))
+            {
+                return RedirectToAction("Index", "Home");
+            }
             return View();
         }
 
