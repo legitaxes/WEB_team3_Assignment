877ae91e686bb9d95b44712781a3d6bd483cf5fd
Mingyi Brandon Lim <s10189534@connect.np.edu.sg>
2019-06-26 14:06:36 +0800

    SkillSetDAL :)

---
+++ web_team3_assignment/DAL/SkillSetDAL.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Data;
+using System.Data.SqlClient;
+using web_team3_assignment.Models;
+
+namespace web_team3_assignment.DAL
+{
+    public class SkillSetDAL
+    {
+        private IConfiguration Configuration { get; set; }
+        private SqlConnection conn;
+        public SkillSetDAL()
+        {
+            //Locate the appsettings.json file
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+            //Read ConnectionString from appsettings.json file
+            Configuration = builder.Build();
+            string strConn = Configuration.GetConnectionString(
+            "Student_EportfolioConnectionString");
+            //Instantiate a SqlConnection object with the
+            //Connection String read.
+            conn = new SqlConnection(strConn);
+        }
+
+        public List<SkillSet> GetAllSkillSet()
+        {
+            //Instantiate a SqlCommand object, supply it with a
+            //SELECT SQL statement that operates against the database,
+            //and the connection object for connecting to the database.
+            SqlCommand cmd = new SqlCommand(
+            "SELECT * FROM Skillset ORDER BY SkillsetID", conn);
+            //Instantiate a DataAdapter object and pass the
+            //SqlCommand object created as parameter.
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            //Create a DataSet object to contain records get from database
+            DataSet result = new DataSet();
+            //Open a database connection
+            conn.Open();
+            //Use DataAdapter, which execute the SELECT SQL through its
+            //SqlCommand object to fetch data to a table "StaffDetails"
+            //in DataSet "result".
+            da.Fill(result, "SkillsetDetails");
+            //Close the database connection
+            conn.Close();
+
+            //Transferring rows of data in DataSet’s table to “Staff” objects
+            List<SkillSet> SkillSetList = new List<SkillSet>();
+            foreach (DataRow row in result.Tables["LecturerDetails"].Rows)
+            {
+                SkillSetList.Add(
+                new SkillSet
+                {
+                    SkillSetId = Convert.ToInt32(row["SkillsetID"]),
+                    SkillSetName = row["SkillSet Name"].ToString()
+                }
+                );
+            }
+            return SkillSetList;
+        }
+    }
+}
