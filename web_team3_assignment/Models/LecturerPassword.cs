faaeb416ea938faebde274800990e18f88cc3c7a
Jia Qi Poh <s10189672@connect.np.edu.sg>
2019-07-04 01:28:34 +0800

    Added MinimumString Length For Password Change Field
    Cleaned up the error message in the Change.cshtml View

--- web_team3_assignment/Models/LecturerPassword.cs
+++ web_team3_assignment/Models/LecturerPassword.cs
@@ -19,14 +19,14 @@ namespace web_team3_assignment.Models
         [Display(Name = "New Password:")]
         [DataType(DataType.Password)]
         [StringLength(255, ErrorMessage = "Password Length Cannot Exceed 255 Characters!")]
-        //[MinLength(8, ErrorMessage = "Password Should Be At least 8 Characters Long!")]
+        [MinLength(8, ErrorMessage = "Password Should Be At least 8 Characters Long!")]
         public string NewPassword { get; set; }
 
         [Required(ErrorMessage = "Please Do not Leave This Field Blank!")]
         [Display(Name = "Confirm Password:")]
         [DataType(DataType.Password)]
         [StringLength(255, ErrorMessage = "Password Length Cannot Exceed 255 Characters!")]
-        //[MinLength(8, ErrorMessage = "Password Should Be At least 8 Characters Long!")]
+        [MinLength(8, ErrorMessage = "Password Should Be At least 8 Characters Long!")]
         public string ConfirmPassword { get; set; }
 
 
