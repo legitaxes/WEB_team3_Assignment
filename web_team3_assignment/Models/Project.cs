3d7dffa48dc21fc606223a09dec0bb97cb61bf63
weilinchoo <s10189897@connect.np.edu.sg>
2019-07-09 19:29:27 +0800

    Add Validation for project Title: ProjectDAL (Edited) Project (Edited) ValidateTitleExists (Added)

--- web_team3_assignment/Models/Project.cs
+++ web_team3_assignment/Models/Project.cs
@@ -16,6 +16,8 @@ namespace web_team3_assignment.Models
 
         [Required(ErrorMessage = "Please Do not Leave Title Field Blank!")]
         [StringLength(255, ErrorMessage = "Project Title Cannot Exceed 255 Characters!")]
+        // Custom Validation Attribute for checking SkillSet exists
+        [ValidateTitleExists(ErrorMessage = "Title already exists!")]
         [Display(Name = "Title")]
         public string Title { get; set; }
 
---
+++ web_team3_assignment/Models/ValidateTitleExists.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using web_team3_assignment.DAL;
+
+namespace web_team3_assignment.Models
+{
+    public class ValidateTitleExists : ValidationAttribute
+    {
+        private ProjectDAL projectContext = new ProjectDAL();
+        public override bool IsValid(object value)
+        {
+            string Title = Convert.ToString(value);
+            if (projectContext.IsProjectTitleExists(Title))
+                return false; // validation failed
+            else
+                return true; // validation passed 
+        }
+    }
+}
